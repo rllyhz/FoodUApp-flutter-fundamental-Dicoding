3cb98b403f8f5f0a9459caeedb17b276501e797c
Rully Ihza Mahendra <rullyihza00@gmail.com>
2021-08-19 18:40:52 +0700

    Updating main page file

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_app/widgets/custom_bottom_nav_bar.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key, required this.title}) : super(key: key);
@@ -11,6 +12,11 @@ class MainPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
+      ),
+      bottomNavigationBar: CustomBottomNavBar(
+        onTabCallback: (index) {
+          print("clicked $index");
+        },
       ),
       body: Center(
         child: Text("Scratch"),
