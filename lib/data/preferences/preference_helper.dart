86ff575e13f04fe1b7a3518564494e8fccd66455
Rully Ihza Mahendra <rullyihza00@gmail.com>
2021-09-01 22:24:53 +0700

    Creating preference_helper.dart file

--- lib/data/preferences/preference_helper.dart
+++ lib/data/preferences/preference_helper.dart
@@ -1,18 +1,29 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-class PrefereceHelper {
+class PreferenceHelper {
   final Future<SharedPreferences> sharedPreferences;
   static const DAILY_RECOMMENDATION_KEY = 'DAILY_RECOMMENDATION_KEY';
+  static const USERNAME_KEY = 'USERNAME_KEY';
 
-  PrefereceHelper({required this.sharedPreferences});
+  PreferenceHelper({required this.sharedPreferences});
 
-  Future<bool> get shouldDailyRecommendationShow async {
+  Future<bool> get shouldDailyRecommendationBeNotified async {
     final prefs = await sharedPreferences;
     return prefs.getBool(DAILY_RECOMMENDATION_KEY) ?? false;
   }
 
-  void setShouldDailyRecommendationShow(bool value) async {
+  void setShouldDailyRecommendationBeNotified(bool value) async {
     final prefs = await sharedPreferences;
     prefs.setBool(DAILY_RECOMMENDATION_KEY, value);
+  }
+
+  Future<String?> get username async {
+    final prefs = await sharedPreferences;
+    return prefs.getString(USERNAME_KEY);
+  }
+
+  void setUsername(String username) async {
+    final prefs = await sharedPreferences;
+    prefs.setString(USERNAME_KEY, username);
   }
 }
