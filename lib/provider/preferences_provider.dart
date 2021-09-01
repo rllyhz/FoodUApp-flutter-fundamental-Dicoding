292df9f740dd9d84178c8dca2441119479583c26
Rully Ihza Mahendra <rullyihza00@gmail.com>
2021-09-01 23:59:46 +0700

    Updating preferences_provider.dart file

--- lib/provider/preferences_provider.dart
+++ lib/provider/preferences_provider.dart
@@ -1,10 +1,15 @@
+import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:restaurant_app/data/preferences/preference_helper.dart';
+import 'package:restaurant_app/utils/background_service.dart';
+import 'package:restaurant_app/utils/generator.dart';
 
 class PreferencesProvider extends ChangeNotifier {
   final PreferenceHelper preferencesHelper;
+
+  static final alarmId = 123;
 
   PreferencesProvider({required this.preferencesHelper}) {
     _getShouldDailyRecommendationShow();
@@ -28,14 +33,25 @@ class PreferencesProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  void setShouldDailyRecommendationBeNotified(bool shouldBeNotified) {
+  void setShouldDailyRecommendationBeNotified(bool shouldBeNotified) async {
     preferencesHelper.setShouldDailyRecommendationBeNotified(shouldBeNotified);
     _shouldDailyRecommendationBeNotified = shouldBeNotified;
     notifyListeners();
 
     if (shouldBeNotified) {
+      await AndroidAlarmManager.periodic(
+        Duration(hours: 24),
+        alarmId,
+        BackgroundService.callback,
+        startAt: getDailyNotificationTime(),
+        exact: true,
+        wakeup: true,
+      );
+
       Fluttertoast.showToast(msg: 'Reminder\'s successfully enabled!');
     } else {
+      await AndroidAlarmManager.cancel(alarmId);
+
       Fluttertoast.showToast(msg: 'Reminder\'s successfully disabled!');
     }
   }
